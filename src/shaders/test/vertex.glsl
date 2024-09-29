e63dadeb53a9b1ee2e9b1b551ac75147af1fcc09
lou <dustyfingers01257@gmail.com>
2024-09-29 10:46:10 -0600

    learn about varyings by colorizinng the randomized mesh

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -5,6 +5,8 @@
     attribute vec3 position;
     attribute float aRandom;
 
+    varying float vRandom;
+
     void main()
     {
         vec4 modelPosition = modelMatrix * vec4(position, 1.0);
@@ -12,4 +14,6 @@
         vec4 viewPosition = viewMatrix * modelPosition;
         vec4 projectedPosition = projectionMatrix * viewPosition;
         gl_Position = projectedPosition;
+
+        vRandom = aRandom;
     }
